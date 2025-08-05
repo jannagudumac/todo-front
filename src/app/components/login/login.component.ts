98e5c4d0d98ab5377c1b1cafa3e81f4339db8058
triceratops777 <jgood.ulysses@gmail.com>
2025-08-05 11:21:02 +0200

    change login component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../../services/auth.service';
@@ -56,4 +56,49 @@ export class LoginComponent implements OnInit {
   });
 
 }
+}*/
+
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+
+
+@Component({
+  selector: 'app-login',
+  standalone: false,
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  // Ajout d'une variable pour gérer les messages d'erreur
+  errorMessage: string = '';
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService, // <-- Injection du service d'authentification
+    private router: Router
+  ) {}
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      // J'ai renommé 'username' en 'email' pour correspondre à ce que le backend attend généralement
+      username: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const credentials = this.loginForm.value;
+      this.authService.login(credentials).subscribe({
+        next: (res) => {
+          sessionStorage.setItem('authToken', res.token);
+          this.router.navigateByUrl('');
+        },
+        error: (err) => console.error('Erreur de connexion', err),
+      });
+    }
+  }
 }
